ce2e9fd2ea67fa2bf945037bde137ce4ea6782dd
nfoissac <nicola.foissac@madintec.com>
2021-03-22 16:34:45 +0100

    feat: add simple csv decoder for V2 result
    #3

--- src/BoostSupport.cxx
+++ src/BoostSupport.cxx
@@ -37,14 +37,132 @@ namespace influxdb::internal
         std::stringstream responseString;
         responseString << response;
         std::vector<Point> points;
+        size_t _time = 0L;
+        size_t  _value = 0L;
+        size_t _field = 0L;
+        size_t _measurement = 0L;
+        auto count = 0L;
+        size_t count_row = 0L;
+        auto table_count = -1L;
+        bool next_row = false;
+
+        for (std::string line; std::getline(responseString, line); ) {
+
+            if(line.at(0) == '#'){
+                if(line.find("#datatype") != std::string::npos){
+                    _field = 0L;
+                    _time = 0L;
+                    _value = 0L;
+                    _measurement = 0L;
+                    count_row = 0L;
+                }
+                continue;
+            }
+
+            if(line.find(",result,") != std::string::npos){
+                table_count++;
+                std::stringstream header;
+                header << line;
+
+                for ( std::string tok; std::getline(header, tok,',');){
+                    if(tok.find("_time") != std::string::npos){
+                        _time = count_row;
+                    }
+
+                    if(tok.find("_value") != std::string::npos){
+                        _value = count_row;
+                    }
+
+                    if(tok.find("_field") != std::string::npos){
+                        _field = count_row;
+                    }
+
+                    if(tok.find("_measurement") != std::string::npos){
+                        _measurement = count_row;
+                    }
+                    count_row++;
+                }
+                continue;
+            }
+
+            count_row = 0;
+            std::stringstream header;
+            header << line;
+
+            std::string fields;
+            std::string name;
+            double value = {};
+            std::tm tm = {};
+
+            for (std::string tok; std::getline(header, tok, ',');)
+            {
+                if (_measurement == count_row)
+                {
+                    name = tok;
+                }
+                if (_field == count_row)
+                {
+                    fields = tok;
+                }
+
+                if (_value == count_row)
+                {
+                    try
+                    {
+                        value = boost::lexical_cast<double>(tok);
+                    }
+                    catch (...)
+                    {
+                    }
+                }
+
+                if (_time == count_row)
+                {
+                    std::stringstream timeString;
+                    timeString << tok;
+                    timeString >> std::get_time(&tm, "%Y-%m-%dT%H:%M:%SZ");
+                }
+
+                if(count_row == 2){
+                    if(table_count != std::stol(tok)){
+                        next_row = true;
+                        count = 0;
+                        table_count++;
+                    }
+                }
+                count_row++;
+            }
+
+            if(line.size() == 1){
+                continue;
+            }
+
+            if(next_row){
+                points.at(count).addField(fields,value);
+            }
+            else{
+                Point p;
+                p.setName(name);
+                p.addField(fields, value);
+                p.setTimestamp(std::chrono::system_clock::from_time_t(std::mktime(&tm)));
+                points.push_back(p);
+            }
+
+            count++;
+        }
+        /*
         boost::property_tree::ptree pt;
         boost::property_tree::read_json(responseString, pt);
-
+        std::cout << responseString.str() << "\n";
         for (const auto& result : pt.get_child("results"))
         {
             const auto isResultEmpty = result.second.find("series");
             if (isResultEmpty == result.second.not_found())
             {
+                const auto isError = result.second.find("error");
+                if(isError != result.second.not_found()){
+                    throw BadRequest(__func__, "Bad request: " + isError->second.get_value<std::string>());
+                }
                 return {};
             }
             for (const auto& series : result.second.get_child("series"))
@@ -79,10 +197,12 @@ namespace influxdb::internal
                             point.addTag(column, value);
                         }
                     }
+
                     points.push_back(std::move(point));
                 }
             }
-        }
+
+        }*/
         return points;
     }
 
